41a54629ef8944cdf4bf147b99ad101a7f4ad258
VaishakNair <vaishak.k.nair@gmail.com>
2024-02-22 10:56:42 +0530

    Finish wiring logic for converting .keras model to .tflite model.

--- tfliteconverter.py
+++ tfliteconverter.py
@@ -3,6 +3,7 @@ import pathlib
 import numpy as np
 from u2net import U2Net
 
+
 IMG_HEIGHT, IMG_WIDTH = U2Net.INPUT_IMAGE_HEIGHT, U2Net.INPUT_IMAGE_WIDTH
 
 
@@ -10,7 +11,7 @@ keras_model_file_name = "40-0.467696.keras"
 keras_model_path = pathlib.Path(
     "/home/vaishak/Downloads/checkpoints-20240221T053611Z-001/checkpoints") / keras_model_file_name
 
-saved_model_save_dir = pathlib.Path("/home/vaishak/Downloads/tflite/u2netlite/saved_model")
+saved_model_dir = pathlib.Path("/home/vaishak/Downloads/tflite/u2netlite/saved_model")
 
 tflite_model_save_dir = pathlib.Path("/home/vaishak/Downloads/tflite/u2netlite")
 
@@ -23,19 +24,22 @@ def get_saved_model_from_keras_model():
 
     # Save as SavedModel:
     saved_model_name = keras_model_file_name.split('-')[0]
-    keras_model.save(saved_model_save_dir / saved_model_name, save_format="tf")
+    keras_model.save(saved_model_dir / saved_model_name, save_format="tf")
 
-    return tf.keras.saving.load_model(saved_model_save_dir / saved_model_name), saved_model_name
+    return tf.keras.saving.load_model(saved_model_dir / saved_model_name), saved_model_name
 
 
 if __name__ == "__main__":
     saved_model, saved_model_name = get_saved_model_from_keras_model()
 
-    # Convert the model
+    # Convert the model to .tflite
     converter = tf.lite.TFLiteConverter.from_saved_model(
-        str(saved_model_save_dir / saved_model_name))  # path to the SavedModel directory
+        str(saved_model_dir / saved_model_name))  # path to the SavedModel directory
     tflite_model = converter.convert()
 
-    # Save the model.
+    # Save the .tflite model.
     with open(tflite_model_save_dir / f"{saved_model_name}.tflite", 'wb') as f:
         f.write(tflite_model)
+
+
+
